e217078d05e8c25bc1cf4840dd09f482602dd31a
lethimythu <mythu.scb@gmail.com>
2019-09-28 20:51:05 +0700

    update code

--- Models/danhSachNhaVien.js
+++ Models/danhSachNhaVien.js
@@ -2,6 +2,6 @@ function DanhSachNhaVien(){
     this.mangSinhVIen = [];
     this.themSinhVien = function (sv) {
         this.mangSinhVIen.push(sv)
-        this.mangSinhVIen.phung khìn;
+        this.ma = "";
     }
 }
